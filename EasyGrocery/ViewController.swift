39b48ec2c541e7ab35ab56b0e07966837edc437d
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-04-12 15:21:50 -0300

    Initial Commit

---
+++ EasyGrocery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyGrocery
+//
+//  Created by Rodrigo Cavalcanti on 12/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
